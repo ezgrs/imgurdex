821b2733d8d04008554017a69252431e67faaad8
Enzo Santos <enzo.santos29@hotmail.com>
2026-04-26 02:36:01 -0300

    feat: splits POST /imgur into two endpoints

--- src/imgurbc/api/app.py
+++ src/imgurbc/api/app.py
@@ -1,5 +1,6 @@
 import random
 import typing
+import http.client
 
 import fastapi
 import httpx
@@ -42,17 +43,9 @@ def _create_consumer() -> Consumer:
 def create_app() -> fastapi.FastAPI:
     app = fastapi.FastAPI()
 
-    @app.post("/imgur")
-    async def _(
-        id_iterator: typing.Annotated[
-            IdIterator, fastapi.Depends(_create_id_iterator)
-        ],
-        consumer: typing.Annotated[Consumer, fastapi.Depends(_create_consumer)],
-        imgur_id: typing.Annotated[str | None, fastapi.Body(embed=True)] = None,
-    ):
-        if imgur_id is None:
-            imgur_id = next(id_iterator)
-
+    async def _fetch_image(
+        consumer: Consumer, imgur_id: str
+    ) -> fastapi.Response:
         async with httpx.AsyncClient() as client:
             downloader = HttpxDownloader(
                 client=client,
@@ -61,7 +54,25 @@ def create_app() -> fastapi.FastAPI:
             resource = await downloader.download(imgur_id)
             if resource is None:
                 await consumer.consume_miss(imgur_id)
-            else:
-                await consumer.consume_hit(resource)
+                return fastapi.Response(status_code=http.client.NOT_FOUND)
+
+            await consumer.consume_hit(resource)
+            return fastapi.Response(status_code=http.client.OK)
+
+    @app.post("/imgur/random")
+    async def fetch_random_imgur_image(
+        id_iterator: typing.Annotated[
+            IdIterator, fastapi.Depends(_create_id_iterator)
+        ],
+        consumer: typing.Annotated[Consumer, fastapi.Depends(_create_consumer)],
+    ) -> fastapi.Response:
+        return await _fetch_image(consumer, next(id_iterator))
+
+    @app.post("/imgur/{imgur_id}")
+    async def fetch_imgur_image(
+        consumer: typing.Annotated[Consumer, fastapi.Depends(_create_consumer)],
+        imgur_id: typing.Annotated[str, fastapi.Path()],
+    ) -> fastapi.Response:
+        return await _fetch_image(consumer, imgur_id)
 
     return app
